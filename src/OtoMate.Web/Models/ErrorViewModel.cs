42ff096cc879972b96e1b005c24a77bedc85b599
Samet Yucel <sametycl02@gmail.com>
2025-09-03 16:05:53 +0300

    Create n-tier architecture

---
+++ src/OtoMate.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OtoMate.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
